3eaea07ddd86be398c72ee8349ec694072cfb5c3
sibandasibusisomduduzi-commits <sibandasibusisomduduzi@gmail.com>
2026-01-06 15:50:19 +0200

    .

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~SYi61j", // Put S
+ession Id Here Inside "*"
+  OWNER_NUMBER: "263788912873", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
